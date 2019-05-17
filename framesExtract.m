56672005f9e2b00953e270e0a20e9494be58ee34
Boscuit <115010184@link.cuhk.edu.c>
2019-05-17 14:57:05 +0800

    finallize

--- framesExtract.m
+++ framesExtract.m
@@ -1,7 +1,7 @@
-obj = VideoReader('D://EIE4512//project//realTest//test (4).mp4');
-for k = 1:90
+obj = VideoReader('D://EIE4512//project//realTest//test (11).mp4');
+for k = 200:299
     frame = read(obj,k);
     frame = imresize(frame,0.5);
-    imwrite(frame,strcat('D:\EIE4512\project\realTest\frames4\',num2str(k),'.jpg'),'jpg');
+    imwrite(frame,strcat('D:\EIE4512\project\realTest\frames11\',num2str(k),'.jpg'),'jpg');
 end
     
--- longexposuresimu.m
+++ longexposuresimu.m
@@ -2,9 +2,9 @@ clear;
 %---------parameter setting---------
 excomprate = 1;
 fuse = 25*excomprate;%bound the extreme optical flow
-testNum = 7;
+testNum = 11;
 superpixel = 500;
-frselect = [280,50];%frnumber[framestart,number]test1[300,50]test3[2,100]test4[60,60]test7[280,50]test8[120,90]test11[2,100]test12[200,100]
+frselect = [2,100];%frnumber[framestart,number]test1[300,50]test3[2,100]test4[60,60]test7[280,50]test8[120,90]test11[2,100]test12[200,100]
 path = strcat('D://EIE4512//project//realTest//test (',num2str(testNum),').mp4');
 obj = VideoReader(path);
 Num = obj.NumberOfFrame;
@@ -32,25 +32,6 @@ for p = 1:frselect(2)-1
     [opticalflow] = getopticalflow_sp(im1,im2,fuse,superpixel); % superpixel
 %     [opticalflow] = getopticalflow_pbp(im1,im2); % pixel to pixel
     of = of + opticalflow;
-
-    % ------------stacking(stack_mean_fbf,stack_max_fbf)----------------
-%     frout = stack_mean_fbf(frout,frlist(:,:,:,p+1),opticalflow(:,:,1),opticalflow(:,:,2),p);
-
-    % downsize u and v
-%     u_deci = opticalflow(1:10:end, 1:10:end, 1);
-%     v_deci = opticalflow(1:10:end, 1:10:end, 2);
-%     % get coordinate for u and v in the original frame
-%     [m, n] = size(im1);
-%     [X,Y] = meshgrid(1:n, 1:m);
-%     X_deci = X(1:10:end, 1:10:end);
-%     Y_deci = Y(1:10:end, 1:10:end);
-%     % Plot optical flow field
-%     figure;
-%     imshow(frlist(:,:,:,p+1));
-%     title(strcat('frame: ',num2str(p+frselect(1)-1),', adpth: ',num2str(adpth)));
-%     hold on;
-%     % draw the velocity vectors
-%     quiver(X_deci, Y_deci, u_deci,v_deci, 'y')
 end
     
 uv2 = of(:,:,1).^2+of(:,:,2).^2;
@@ -64,6 +45,7 @@ th = graythresh(uvim);%see value over 1 as 1
 uvimbw=imbinarize(uvim,th);
 
 midfr = frlist(:,:,:,round(frselect(2)/2));
+%Other function to used for different effect:[stack_min_all,stack_median_all,stack_mean_all,]
 froutmax= stack_max_all(frgraylist,frlist);
 frout = midfr.*(1-uvimr)+froutmax.*uvimr;
 
